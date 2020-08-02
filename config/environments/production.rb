881262d198fe111f8ae92407e8969c4af0178817
Conner Krall <conner.krall@gmail.com>
2020-08-02 22:29:12 +0000

    update font

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'https://random-thoughts-memories.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
