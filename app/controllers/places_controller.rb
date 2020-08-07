490a1e2a485c06d546bd9ce9837f945206d99b6d
Conner Krall <conner.krall@gmail.com>
2020-08-07 18:58:09 +0000

    user authentication for places pages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @places = current_user.places
+    @places = current_user.places.order(start: :desc)
   end
 
   def new
@@ -15,11 +15,15 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+      redirect_to root_path
+    end
   end
 
   private
 
   def place_params
-    params.require(:place).permit(:title, :start, :end)
+    params.require(:place).permit(:start, :location, :title, :end)
   end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,18 @@ class Place < ApplicationRecord
   belongs_to :user
   geocoded_by :location
   after_validation :geocode
+
+  validates :location, :presence => true
+  validates :start, :presence => true
+
+  attr_reader :dates
+
+  def dates
+    if self.end == self.start || self.end.nil?
+      return "#{self.start}"
+    else
+      return "#{self.end} - #{self.start}"
+    end
+  end
+
 end
